c9caf5057d90109a0f9a36876f4b07ec2c95cc85
kaviraj1910 <75670143+kaviraj1910@users.noreply.github.com>
2025-04-05 17:53:45 +0530

    Update code-samples.js

--- code-samples.js
+++ code-samples.js
@@ -1,1 +1,14 @@
+// first sample: to generate an excel file using xlsx and download
+
+const worksheet = xlsx.utils.json_to_sheet(array);
+const workbook = xlsx.utils.book_new();
+xlsx.utils.book_append_sheet(workbook, worksheet, 'Sheet 1');
+const excelBuffer = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+const blob = new Blob([excelBuffer], {type: 'application/octet-stream'});
+const link = document.createElement('a');
+link.href = URL.createObjectURL(blob);
+link.download = 'excel-file.xlsx';
+document.body.appendChild(link);
+link.click();
+document.body.removeChild(link);
 

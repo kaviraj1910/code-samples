560506c5905cfdbe083f07a78ad5d2fbc5c0fb13
kaviraj1910 <75670143+kaviraj1910@users.noreply.github.com>
2025-04-30 10:42:57 +0530

    Update code-samples.js

--- code-samples.js
+++ code-samples.js
@@ -12,3 +12,29 @@ document.body.appendChild(link);
 link.click();
 document.body.removeChild(link);
 
+// second sample: to generate sitemap and download
+
+const sitemapContent = _.map(blogs, blog => {
+            const url = `${baseurl}/${_.lowerCase(blog.type)}/${blog.slug}`;
+            const currentDate = blog.lastUpdatedOn ? blog.lastUpdatedOn : (blog.createdAt ? blog.createdAt : new Date().toISOString());
+            return `<url>
+            <loc>${url}</loc>
+            <lastmod>${currentDate}</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>1.0</priority>
+        </url>`;
+        }).join("");
+
+        const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+            <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            ${sitemapContent}
+            </urlset>
+        `;
+
+        const blob = new Blob([sitemap], {type: 'text/xml'});
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = 'sitemap-blogs.xml';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
